7c08894128b82388eebfb726285fb05dbcad5588
MAD_DRAX <k.d.n8125u@gmail.com>
2025-02-22 12:38:51 +0530

    Updated ALLOWED_HOSTS for Render

--- MusicPlayer/settings.py
+++ MusicPlayer/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-0e1x$yen^jm@wiho+_%!dtau-(&dp6x-=pml3m5^$zbzyb&---
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
+
 
 
 # Application definition
